a43587b8c5681c82379f43b9442d9481b98b8cde
Forhad Reza <forhad.bimt@gmail.com>
2025-12-10 11:04:57 +0600

    Refactor photo API route and update dependencies

--- app/api/photos/[id]/route.ts
+++ app/api/photos/[id]/route.ts
@@ -1,9 +1,12 @@
-import { getPhotoById } from "@/lib/image-data";
-import { NextResponse } from "next/server";
+import { getPhotoById } from '@/lib/image-data';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
-//   console.log("params", params);
-  const { id } = await params;
-  const photoDetails = await getPhotoById(id);
-  return NextResponse.json(photoDetails);
+export async function GET(
+   req: Request,
+   { params }: { params: Promise<{ id: string }> }
+) {
+   //   console.log("params", params);
+   const { id } = await params;
+   const photoDetails = await getPhotoById(id);
+   return Response.json(photoDetails);
 }
